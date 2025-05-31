f8c0ae132fc53abd56a6dc12eade40f074d4f22d
BuildTools <huxk2008@163.com>
2025-05-31 15:25:59 +0800

    Another shit commit

--- translator/skripter.py
+++ translator/skripter.py
@@ -1,4 +1,19 @@
+from io import TextIOWrapper
 from utils.bukkit_material import BukkitMaterial
 from utils.misc import Enchantment
 from utils.custom_item import CustomItem
 from utils.custom_recipe import CustomRecipe
+
+import json
+import tomllib
+import yaml
+
+ACCEPTABLE_EXTENSIONS = ('.json', '.toml', '.yml', '.yaml')
+
+def read_file(file: TextIOWrapper):
+    """
+    Reads a file and returns its content as a dictionary.
+    Supports JSON, TOML, and YAML formats.
+    """
+    # TODO: 不知道要写什么
+    pass
